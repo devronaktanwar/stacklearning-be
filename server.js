3f5d6a5b49ba65f014d6b0b51d7bcd90ef6a1f7f
ronaktanwar <179121774+ronaktanwar@users.noreply.github.com>
2024-10-30 17:58:12 +0530

    fix cors

--- server.js
+++ server.js
@@ -4,23 +4,15 @@ const jobRoutes = require("./routes/jobs");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 4000;
 const cors = require("cors");
-const allowedOrigins = ["https://stacklearning.in", "http://localhost:5173"];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST","DELETE"],
-  allowedHeaders: ["authkey", "Content-Type"],
-};
 connectDB();
-// app.use(cors(corsOptions));
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "https://www.stacklearning.in/", 
+    methods: ["GET", "POST", "DELETE"], 
+    allowedHeaders: ["Content-Type", "authkey"], 
+  })
+);app.use(express.json());
 app.use("/api", jobRoutes);
 
 app.listen(port, () => {
