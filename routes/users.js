6985e59c944c375a6babcce5a994a0edd2e67cd5
ronaktanwar <179121774+ronaktanwar@users.noreply.github.com>
2024-10-31 13:13:38 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,9 @@ router.post("/signup", authMiddleware, async (req, res) => {
     });
 
     await newUser.save();
-    res.status(201).json({ message: "User created successfully", newUser });
+    res
+      .status(201)
+      .json({ isSuccess: true, message: "User created successfully", newUser });
   } catch (error) {
     res.status(500).json({ message: error });
   }
@@ -32,30 +34,34 @@ router.post("/signup", authMiddleware, async (req, res) => {
 router.post("/login", async (req, res) => {
   const { emailAddress, passWord } = req.body;
 
-  try{
+  try {
     const existingUser = await User.findOne({ emailAddress });
-    if(!existingUser){
-        return res.status(400).json({
-            isSuccess: false,
-            message: "Invaild Email, Please Sign Up",
-          });
+    if (!existingUser) {
+      return res.status(400).json({
+        isSuccess: false,
+        message: "Invaild Email, Please Sign Up",
+      });
     }
-    const isPasswordValid = await bcrypt.compare(passWord, existingUser.passWord);
+    const isPasswordValid = await bcrypt.compare(
+      passWord,
+      existingUser.passWord
+    );
 
     if (!isPasswordValid) {
-        return res.status(400).json({
-          isSuccess: false,
-          message: "Invalid password",
-        });
-      }
-      res.status(200).json({
-        isSuccess: true,
-        message: "Login successful",
-        user: { id: existingUser._id, emailAddress: existingUser.emailAddress, fullName: existingUser.fullName },
+      return res.status(400).json({
+        isSuccess: false,
+        message: "Invalid password",
       });
-  }
-  catch(err){
-
-  }
+    }
+    res.status(200).json({
+      isSuccess: true,
+      message: "Login successful",
+      user: {
+        id: existingUser._id,
+        emailAddress: existingUser.emailAddress,
+        fullName: existingUser.fullName,
+      },
+    });
+  } catch (err) {}
 });
 module.exports = router;
