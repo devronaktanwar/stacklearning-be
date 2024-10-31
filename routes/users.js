f8315faa4de340e2d63b62234f1da3f728e16b01
ronaktanwar <179121774+ronaktanwar@users.noreply.github.com>
2024-10-31 13:02:06 +0530

    add login route

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,6 @@ router.post("/signup", authMiddleware, async (req, res) => {
   const { fullName, emailAddress, passWord } = req.body;
   try {
     const existingUser = await User.findOne({ emailAddress });
-    console.log("existingUser",existingUser)
     if (existingUser) {
       return res.status(400).json({
         isSuccess: false,
@@ -19,15 +18,44 @@ router.post("/signup", authMiddleware, async (req, res) => {
     const hashedPassword = await bcrypt.hash(passWord, 10);
 
     const newUser = new User({
-        fullName,
-        emailAddress,
-        passWord:hashedPassword
-      });
+      fullName,
+      emailAddress,
+      passWord: hashedPassword,
+    });
 
-      await newUser.save();
-      res.status(201).json({ message: "User created successfully", newUser });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully", newUser });
   } catch (error) {
-    res.status(500).json({ message: error});
+    res.status(500).json({ message: error });
+  }
+});
+router.post("/login", async (req, res) => {
+  const { emailAddress, passWord } = req.body;
+
+  try{
+    const existingUser = await User.findOne({ emailAddress });
+    if(!existingUser){
+        return res.status(400).json({
+            isSuccess: false,
+            message: "Invaild Email, Please Sign Up",
+          });
+    }
+    const isPasswordValid = await bcrypt.compare(passWord, existingUser.passWord);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({
+          isSuccess: false,
+          message: "Invalid password",
+        });
+      }
+      res.status(200).json({
+        isSuccess: true,
+        message: "Login successful",
+        user: { id: existingUser._id, emailAddress: existingUser.emailAddress, fullName: existingUser.fullName },
+      });
+  }
+  catch(err){
+
   }
 });
 module.exports = router;
