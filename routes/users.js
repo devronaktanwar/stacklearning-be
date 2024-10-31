cfb1ab6543ac073d5b0f139f45f4bdacd2d552e7
ronaktanwar <179121774+ronaktanwar@users.noreply.github.com>
2024-10-31 19:23:41 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.post("/signup", authMiddleware, async (req, res) => {
       .status(201)
       .json({ isSuccess: true, message: "User created successfully", newUser });
   } catch (error) {
-    res.status(500).json({ message: error });
+    res.status(500).json({ isSuccess: false, message: error });
   }
 });
 router.post("/login", async (req, res) => {
