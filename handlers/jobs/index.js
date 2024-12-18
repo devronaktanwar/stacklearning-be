bb8f2e8d81680cf936db854a100e09a159e4c42a
ronaktanwar <179121774+ronaktanwar@users.noreply.github.com>
2024-12-18 22:12:11 +0530

    fix search api

--- handlers/jobs/index.js
+++ handlers/jobs/index.js
@@ -3,15 +3,21 @@ const Job = require("../../models/job");
 module.exports = {
   async getJobResults({ keyword, location }) {
     try {
-      const jobs = await Job.find({
-        jobTitle: { $regex: keyword, $options: "i" },
-        location: location,
-      });
-
+      const query = {};
+      if (keyword) {
+        query.jobTitle = { $regex: keyword, $options: "i" }; 
+      }
+      if (location) {
+        query.location = location;
+      }
+  
+      const jobs = await Job.find(query);
+  
       return { isSuccess: true, data: jobs };
     } catch (err) {
       console.error("Error fetching job results:", err);
       return { isSuccess: false, error: err.message };
     }
-  },
+  }
+  
 };
